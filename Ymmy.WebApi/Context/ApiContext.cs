bdd6aa8445bef3825c0386b1f3db28ffa848de71
Furkan ERİÇ <furkaneric9@gmail.com>
2025-09-28 19:21:44 +0300

    Add project files.

---
+++ Ymmy.WebApi/Context/ApiContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using Ymmy.WebApi.Entities;
+
+namespace Ymmy.WebApi.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;initial catalog=ApiYummy;integrated security=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Chef> Chefs { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
+
---
+++ Ymmy.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ymmy.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly Context.ApiContext _context;
+        public CategoriesController(Context.ApiContext context)
+        {
+            _context = context;
+        }
+    }
+}
