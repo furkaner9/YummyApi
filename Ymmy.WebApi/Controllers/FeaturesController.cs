b7d6a712e8f232ba215991e2f60ff7d1a85d3157
Furkan ERİÇ <furkaneric9@gmail.com>
2025-09-29 22:51:39 +0300

    GeneralMapping

---
+++ Ymmy.WebApi/Controllers/FeaturesController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ymmy.WebApi.Context;
+using Ymmy.WebApi.Dtos.FeatureDtos;
+using Ymmy.WebApi.Entities;
+
+namespace Ymmy.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ApiContext _contex;
+        public FeaturesController(IMapper mapper, ApiContext contex)
+        {
+            _mapper = mapper;
+            _contex = contex;
+        }
+        [HttpGet]
+        public IActionResult FeatureList()
+        {
+            var values = _contex.Features.ToList();
+            return Ok(_mapper.Map<List<ResultFeatureDtos>>(values));
+
+        }
+        [HttpPost]
+        public IActionResult CreateFeature(CreateFeatureDtos createFeatureDtos)
+        {
+            var value = _mapper.Map<Feature>(createFeatureDtos);
+            _contex.Features.Add(value);
+            _contex.SaveChanges();
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteFeature(int id)
+        {
+            var value = _contex.Features.Find(id);
+            if (value == null)
+            {
+                return NotFound();
+            }
+            _contex.Features.Remove(value);
+            _contex.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
---
+++ Ymmy.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using AutoMapper.Features;
+using System.Runtime;
+using Ymmy.WebApi.Dtos.FeatureDtos;
+using Ymmy.WebApi.Dtos.MessageDtos;
+using Ymmy.WebApi.Entities;
+
+namespace Ymmy.WebApi.Mapping
+{
+    public class GeneralMapping :Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Feature,ResultFeatureDtos>().ReverseMap();
+            CreateMap<Feature,CreateFeatureDtos>().ReverseMap();
+            CreateMap<Feature,GetByIdFeatureDtos>().ReverseMap();
+            CreateMap<Feature,UpdateFeatureDtos>().ReverseMap();
+            
+            
+            
+            CreateMap<Message,UpdateMessageDtos>().ReverseMap();
+            CreateMap<Message,CreateMessageDtos>().ReverseMap();
+            CreateMap<Message,GetByIdMessageDtos>().ReverseMap();
+            CreateMap<Message,ResultMessageDtos>().ReverseMap();
+
+
+
+        }
+    }
+}
