b2e9467149d34fc6a3e321f1f84dc8202a2891dd
Furkan ERİÇ <furkaneric9@gmail.com>
2025-10-04 22:33:56 +0300

    viewComponent devam

---
+++ Yummy.WebUI/ViewComponents/_FeatureDefaıltComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yummy.WebUI.ViewComponents
+{
+    public class _FeatureDefaıltComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
